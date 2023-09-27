86845601882278eb50de8bb53d908112b91426c7
AmulGupta <126615115+Amulgupta@users.noreply.github.com>
2023-09-27 14:07:02 +0530

    Add files via upload

---
+++ Day-1/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int X;
+	    cin>>X;
+	    if(X>100)
+	    {
+	        X -= 10;
+	    }
+	    cout<<X<<endl;
+	}
+	return 0;
+}
