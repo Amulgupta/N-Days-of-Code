2ba8fb496c3340389377168a2342fb1310520636
AmulGupta <126615115+Amulgupta@users.noreply.github.com>
2023-09-29 15:35:09 +0530

    Add files via upload

---
+++ Day-3/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int X;
+	    cin>>X;
+	    if(X % 4  == 0)
+	    cout<<"Good"<<endl;
+	    else
+	    cout<<"Not Good"<<endl;
+	}
+	return 0;
+}
