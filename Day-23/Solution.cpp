dc76437b87c95a36d16abb3b20560fb1aa4a0445
AmulGupta <126615115+Amulgupta@users.noreply.github.com>
2023-10-19 20:20:57 +0530

    Add files via upload

---
+++ Day-23/Solution.cpp
@@ -0,0 +1,86 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int T;
+// 	cin>>T;
+// 	while(T--)
+// 	{
+// 	    int N;
+// 	    cin>>N;
+// 	    int A[N];
+// 	    for(int i = 0; i < N; i++)
+// 	    {
+// 	        cin>>A[i];
+// 	    }
+// 	    for(int i = 0; i < N - 1; i+2)   // If i < N (condition) is used then we will not be able to access A[N + 1] later.
+// 	    {
+// 	        if(i % 2 == 0 && A[i] > A[i+1]){
+// 	            int temp = A[i];
+// 	            A[i] = A[i + 1];
+// 	            A[i + 1] = temp;
+// 	        }
+// 	        else if(A[i] < A[i+1]){
+// 	            int temp = A[i];
+// 	            A[i + 1] = temp;
+// 	        }
+// 	    }
+// 	    for(int i= 0; i < N; i++)
+// 		{
+// 			cout<<A[i]<<" ";
+// 		}
+// 		cout<<endl;
+// 	}
+// 	return 0;
+// }
+//  Problem with the solution is Time limit exceeded because of three for loops.
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int N;
+	    cin>>N;
+	    int arr[N];
+	    
+	    //getting the array input
+	    for(int i = 0 ; i < N; i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    
+	   // sorting the array
+	    sort(arr,arr + N);
+	    
+	   // swapping the elements in the fashion asked in the question
+	    for(int i = 0 ; i < N  - 1 ; i++)
+	    {
+	        if(i % 2 == 0 && arr[i] > arr[i+1])
+	        {
+	            swap(arr[i],arr[i+1]);
+	        }
+	        if(i % 2 == 1 && arr[i] < arr[i+1])
+	        {
+	            swap(arr[i],arr[i+1]);
+	        }
+	    }
+	    
+	   // printing the array
+	    for(int i = 0 ; i < N; i++)
+	    {
+            cout<<arr[i]<<" ";
+	    }
+
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
